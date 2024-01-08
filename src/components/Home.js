c95a4e45dc6503f0833890337fa3aaefa2d167e5
Ryan Oberly <ryanjoberly@gmail.com>
2024-01-07 20:24:06 -0700

    slightly changed home page description

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home () {
     return (
         <div className="home-container">
             <h1>Welcome to the Culinary Compass</h1>
-            <h2>This is a fun way to learn to cook dishes from around the world!</h2>
+            <h2>Learn to cook dishes from around the world!</h2>
             <h3>Click the 'Compass' button in the nav bar to discover a new recipes from around the world!</h3>
             <div onClick={() => navigate('/compass')} className="home-nav-to-compass">Go to Compass</div>
         </div>
