2edcb906d25af26b8bb894f70af382f36c612d90
Ryan Oberly <ryanjoberly@gmail.com>
2024-01-17 16:14:35 -0700

    returned to auth0-recommended returnTo location

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,11 +1,16 @@
 import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+
+// when in local environment, still logging out to vercel url. there doesn't seem to be a fix. best to quit coding altogether
 const Logout = () => {
   const { logout } = useAuth0();
   useEffect(() => {
-    console.log('env variable', process.env.REACT_APP_BASE_URL)
-    logout({ returnTo: process.env.REACT_APP_BASE_URL })
+    logout({
+      returnTo: window.location.origin, 
+      client_id: 'MjWo6ad1gPBTpRUA2JLTqsiNBfVHf7SJ' 
+    });
+    
   }, [])
   return (
     <div>
