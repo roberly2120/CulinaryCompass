6179c8568cfe5400af25af4073530dbb83d40abc
Ryan Oberly <ryanjoberly@gmail.com>
2024-02-28 19:27:06 -0700

    changed about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,27 @@
 import React from "react";
-
+import { Box, VStack, Heading, Text, Center } from "@chakra-ui/react";
 export default function About () {
     return (
-        <div className="about-container">
-            <h1>About Culinary Compass</h1>
-            <p>I don't know about you, but I thrive on a new cooking challenge. If I have a free day, one of my favorite things is to find a recipe I've never made before and take a crack at it. The purpose of this site is to click one button and discover a new recipe to try. No extra work searching for something to cook...just click the button and go for it!</p>
-        </div>
+        <>
+            <Center>
+                <VStack spacing={4} align="center" justify="start" height="100vh" mt={5}>
+                    <Heading as="h1" size="xl" textAlign="center">
+                        This App is Silly!
+                    </Heading>
+                    <Text fontSize="lg" textAlign="center">
+                        That's actually the point. It's a fun way to learn new recipes from around the world. 
+                    </Text>
+                    <Text fontSize="lg">
+                        So just try it out
+                    </Text>
+                    <Text fontSize="md">
+                        Come on
+                    </Text>
+                    <Text fontSize="sm">
+                        You know you want to   
+                    </Text>
+                </VStack>
+            </Center>
+        </>
     )
 }
