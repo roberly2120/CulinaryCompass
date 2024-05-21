e45a5a82d7c158e943acdb56084a2c0ee77b711a
Ryan Oberly <ryanjoberly@gmail.com>
2024-05-21 17:19:11 -0600

    fixed card spacing/grid on MyRecipes page

--- src/components/MyRecipes.js
+++ src/components/MyRecipes.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { readUserDocuments, deleteDocument } from "../FireStore/firestoreOperations";
 import { useAuth0 } from "@auth0/auth0-react";
-import { VStack, Heading, Center, Spacer, Button, Stack, HStack, StackDivider, Card, Box, CardBody, Text, Spinner } from "@chakra-ui/react";
+import { VStack, Heading, Stack, Center, Spacer, Button, HStack, StackDivider, Card, Box, Flex, CardBody, Text, Spinner, SimpleGrid, useMediaQuery} from "@chakra-ui/react";
 
 export default function MyRecipes() {
     const [recipes, setRecipes] = useState([]);
@@ -10,6 +10,23 @@ export default function MyRecipes() {
     const { user } = useAuth0();
     const navigate = useNavigate();
     const [hasFetched, setHasFetched] = useState(false);
+    
+    const [isLargerThan627, isLargerThan932, isLargerThan1230] = useMediaQuery([
+        "(min-width: 627px)",
+        "(min-width: 932px)",
+        "(min-width: 1230px)",
+    ])
+    let columns;
+    if (isLargerThan1230) {
+        columns = 4;
+    } else if (isLargerThan932) {
+        columns = 3;
+    } else if (isLargerThan627) {
+        columns = 2;
+    } else {
+        columns = 1;
+    }
+
 
     useEffect(() => {
         const fetchRecipes = async () => {
@@ -28,7 +45,7 @@ export default function MyRecipes() {
         setPageNotice('Recipe Deleted')
         setTimeout(() => setPageNotice(''), 5000)
     }
-    if(!hasFetched){
+    if (!hasFetched) {
         return (
             <VStack spacing={4} align="center" justify="center" height="100vh">
                 <Spinner size="xl" />
@@ -55,43 +72,53 @@ export default function MyRecipes() {
                 My Recipes
             </Heading>
             <Text className="page-notice" color="gray.500">{pageNotice}</Text>
-            {recipes.map((recipe, index) => (
-                <Center key={index}>
-                    <Card
-                        w="300px"
-                        h="auto"
-                        minWidth="300px"
-                        minHeight="200px"
-                        mb={5}
-                        border="2px"
-                        borderColor="gray.300"
-                        shadow="lg"
-                    >
-                        <CardBody>
-                            <Stack
-                                divider={<StackDivider borderColor="gray.200" borderWidth="2px" />}
-                                spacing={3}
-                            >
-                                <HStack spacing={4}>
-                                    <Text fontWeight="bold">{recipe.dish}</Text>
-                                    <Box height="1.5rem" width="2px" bgColor="gray.200" alignSelf="center" />
-                                    <Spacer />
-                                    <Text>{recipe.country}</Text>
-                                </HStack>
+            <SimpleGrid columns={columns} spacing={4}>
+                {recipes.map((recipe, index) => (
 
-                                <Text>{recipe.description}</Text>
+                    <Center key={index}>
+                        <Card
+                            w={{ base: "90%", sm: "300px" }}
+                            h={{ base: "auto", sm: "350px" }}
+                            minWidth="300px"
+                            minHeight="200px"
+                            mb={5}
+                            border="2px"
+                            borderColor="gray.300"
+                            shadow="lg"
+                        >
 
-                                <HStack spacing={4}>
-                                    <Button size="md" colorScheme="teal" onClick={() => navigate(`/saved-recipe/${recipe.id}`)}>View Recipe</Button>
-                                    <Box ml={5} height="20px" width="2px" bgColor="gray.200" alignSelf="center" />
-                                    <Spacer />
-                                    <Button size="sm" colorScheme="red" onClick={() => handleDeleteRecipe(recipe.id)}>Delete</Button>
-                                </HStack>
-                            </Stack>
-                        </CardBody>
-                    </Card>
-                </Center>
-            ))}
+                            <CardBody>
+
+                                <Stack
+                                    height="100%"
+                                    divider={<StackDivider borderColor="gray.200" borderWidth="2px" />}
+                                    spacing={3}
+                                >
+                                    <HStack spacing={4}>
+                                        <Text fontWeight="bold">{recipe.dish}</Text>
+                                        <Box height="1.5rem" width="2px" bgColor="gray.200" alignSelf="center" />
+                                        <Spacer />
+                                        <Text>{recipe.country}</Text>
+                                    </HStack>
+
+                                    <Text>{recipe.description}</Text>
+
+
+                                    <Flex flexDirection="column" justifyContent="flex-end" height="100%">
+                                        <Button size="md" colorScheme="teal" onClick={() => navigate(`/saved-recipe/${recipe.id}`)}>View Recipe</Button>
+                                        {/* <Box ml={5} height="20px" width="2px" bgColor="gray.200" alignSelf="center" /> */}
+                                        {/* <Spacer /> */}
+                                        <Button size="sm" colorScheme="red" mt={3} onClick={() => handleDeleteRecipe(recipe.id)}>Delete</Button>
+                                    </Flex>
+                                </Stack>
+
+                            </CardBody>
+
+                        </Card>
+                    </Center>
+
+                ))}
+            </SimpleGrid>
         </VStack>
     );
 }
